1459d8dd402b54497e130b7da77e6348ddbe4f50
Mark Hansen <markdavidhansen@gmail.com>
2016-04-09 15:03:09 -0600

    Added Lab 4 model data
    This *should* launch.

--- src/cs355/model/scene/Line3D.java
+++ src/cs355/model/scene/Line3D.java
@@ -1,23 +1,25 @@
 package cs355.model.scene;
 
 /**
- * A simple line used in making models.
+ *
+ * @author Brennan Smith
  */
-public class Line3D {
-
+public class Line3D 
+{
 	// The start point of the line.
-	public Point3D start;
+    public Point3D start;
 
-	// The ending point of the line.
-	public Point3D end;
+    // The ending point of the line.
+    public Point3D end;
 
 	/**
 	 * Basic constructor.
 	 * @param s = the starting point of the new line.
 	 * @param e = the ending point of the new line.
 	 */
-	public Line3D(Point3D s, Point3D e) {
-		start = s;
-		end = e;
-	}
+    public Line3D(Point3D s, Point3D e)
+    {
+        start = s;
+        end = e;
+    }
 }
